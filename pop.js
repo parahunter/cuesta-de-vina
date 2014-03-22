3de7b80037a1d7fe34124355868197fa182633b0
parahunter <parahunter@gmail.com>
2014-03-22 18:39:30 +0000

    stuff

---
+++ pop.js
@@ -0,0 +1,22 @@
+
+Wortha
+cry
+talk
+
+tasks
+
+seb
+	- make audio list
+	- talk to Cam
+	
+Dan
+	- Wortha
+	- Bertram
+	- conversations
+	
+	- exit house conversation
+Alex
+	- make sure build is playable
+	- give it to seb
+	- give it to Cam
+	
---
+++ timecoding.js
@@ -0,0 +1,26 @@
+var Timecoding = function(start, end, text)
+{
+        this.start = start;
+        this.end = end;
+        this.text = text;
+}
+
+var timecodings = new Array();
+//add new sentences down here 
+timecodings.push( new Timecoding( 0, 3, "When I came to live here in the fifties") );
+timecodings.push( new Timecoding( 3, 8, "actually from another village, really close to this one") );
+timecodings.push( new Timecoding( 8, 10, "not only this village") );
+timecodings.push( new Timecoding( 10, 13, "but the whole region") );
+timecodings.push( new Timecoding( 13, 15, "was completely inhabited, ") );
+timecodings.push( new Timecoding( 15, 19, "because the mines offered") )
+timecodings.push( new Timecoding( 19, 21, "a lot of job opportunities.") )
+timecodings.push( new Timecoding( 21, 24, "In the village there were no abandoned houses.") )
+timecodings.push( new Timecoding( 24, 28, "On  the contrary people lived even in the stables.") )
+timecodings.push( new Timecoding( 28, 31, "The barns were cleaned so people could stay there") )
+timecodings.push( new Timecoding( 31, 37, "And this happened in every single village.") )
+timecodings.push( new Timecoding( 37, 39, "It was not until the sixties") )
+timecodings.push( new Timecoding( 39, 42, "when the economy's growth started ") )
+timecodings.push( new Timecoding( 42, 47, "and other job opportunities appeared, that people earned more money") )
+timecodings.push( new Timecoding( 47, 50, "and they left the villages to live downhill.") )
+timecodings.push( new Timecoding( 50, 53, "But until then, this region was packed.") )
+timecodings.push( new Timecoding( 53, 59, "Nowadays with no people araound, everything is abandoned,") )
